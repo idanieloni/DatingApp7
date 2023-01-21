5dedce8fdc7d2699b1da6e9748c0140742fe2150
idanieloni <idanieloni@gmail.com>
2023-01-20 20:19:19 -0500

    Initial

---
+++ UI/src/app/_forms/text-input/text-input.component.ts
@@ -0,0 +1,31 @@
+import { Component, Input, OnInit, Self } from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.css']
+})
+export class TextInputComponent implements ControlValueAccessor {
+  @Input() label = '';
+  @Input() type = 'text';
+  @Input() minLength: number = 7;
+  @Input() maxLength: number = 15;
+
+  constructor(@Self() public ngControl: NgControl) { 
+    this.ngControl.valueAccessor = this;
+  }
+
+  writeValue(obj: any): void {
+  }
+
+  registerOnChange(fn: any): void {
+  }
+
+  registerOnTouched(fn: any): void {
+  }
+
+  get control(): FormControl {
+    return this.ngControl.control as FormControl
+  }
+}
